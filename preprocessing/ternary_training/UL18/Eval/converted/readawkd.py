b0866730b469a0549a8c2a5fc522853402f7a873
ktauqeer <komaltauqeer3@gmail.com>
2025-07-14 14:06:16 +0200

    Large test files added through LFS

---
+++ preprocessing/ternary_training/UL18/Eval/converted/readawkd.py
@@ -0,0 +1,11 @@
+import awkward as ak
+import pandas
+
+f = ak.load("./Eval_TTCR_TT_UL18_0.awkd")
+#print(f["part_deltaR"])
+#print(f["part_phirel"])
+#print(f["part_etarel"])
+#print(f["part_px"])
+#print(f["part_charge"])
+v1 = print(f["label"])
+print(v1)
